2090e9bc6d134c66c0884fbf95b142aeb45355c9
Zhiqiang Zhang <745124335@qq.com>
2020-04-13 14:32:33 +0800

    init project (#4)

---
+++ internal/types/tags.go
@@ -0,0 +1,15 @@
+// nolint
+package types
+
+import (
+	sdk "github.com/irisnet/irishub/types"
+)
+
+var (
+	TagActionIssueToken = []byte("issue_token")
+
+	TagAction = sdk.TagAction
+	TagSymbol = "token-symbol"
+	TagDenom  = "token-denom"
+	TagOwner  = "token-owner"
+)
